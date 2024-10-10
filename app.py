b15b30f0f12baca2a30b85dde00b67a146a37f7a
arawesome1 <105249379+arawesome1@users.noreply.github.com>
2024-10-10 18:21:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_faiss import FAISS
+from langchain.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
