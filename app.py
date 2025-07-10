5d64daff94db2e96d1e5e25a74376aee8b255f01
arawesome1 <105249379+arawesome1@users.noreply.github.com>
2025-07-10 15:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 os.environ['hf_token']= os.getenv('hf_token')
-embeddings = HuggingFaceEmbeddings(model_name='granite-embedding-30m-sparse')
+embeddings = HuggingFaceEmbeddings(model_name='LaBSE-en-ru')
 st.title("Conversational with PDF")
 st.write("Upload your PDF and Chat with it")
 
